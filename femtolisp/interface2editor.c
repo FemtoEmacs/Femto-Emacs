fd2109d40d0f413da0ada5db92b1ce632ca27c3a
nia <lu.guerra7508@hotmail.com>
2016-08-29 15:14:14 -0300

    K&R style interface2editor

--- femtolisp/interface2editor.c
+++ femtolisp/interface2editor.c
@@ -21,202 +21,235 @@
 /*Interface to the editor */
 
 typedef long point_t;
+
 typedef char *msg_t;
+
 extern msg_t m_sprompt;
 
 char dbg[30];
 
-
 extern int msgflag;
+
 extern char msgline[];
 
-
-static value_t msg_lisp(value_t *args, u_int32_t nargs) {
+static value_t msg_lisp(value_t *args, u_int32_t nargs)
+{
   int i=0;
   argcount("message", nargs, 1);
   value_t a= args[0]; /*Learn: pick an arg */
   char *str= cptr(a); /*Learn: string Lisp -> string C  */
   while (i<80 && str[i] != 0) {
     msgline[i] = str[i];
-    i++ ;}
-  msgline[i]=0;
-  msgflag= 1;
-  return FL_T;}
+    i++ ;
+  }
+    msgline[i]=0;
+    msgflag= 1;
+  return FL_T;
 
+}
 
 extern void insert_string(char *);
-static value_t insrt(value_t *args, u_int32_t nargs) {
+
+static value_t insrt(value_t *args, u_int32_t nargs)
+{
   argcount("insert", nargs, 1);
   value_t a= args[0]; /*Learn: pick an arg */
   char *str= cptr(a); /*Learn: string Lisp -> string C  */
   insert_string(str);
-  return FL_NIL;}
+  return FL_NIL;
+
+}
 
 extern void left(void);
+
 extern void right(void);
 
-void ibchar(long int n) {
+void ibchar(long int n)
+{
   long int i;
   for (i=0; i < n; i++) {
     left();
   }
+  
 }
 
-void ifchar(long int n) {
+void ifchar(long int n)
+{
   long int i;
   for (i=0; i < n; i++) {
     right();
   }
+  
 }
 
-static value_t bkwrd(value_t *args, u_int32_t nargs) {
+static value_t bkwrd(value_t *args, u_int32_t nargs)
+{
   argcount("backward", nargs, 1);
-  value_t a= args[0];
+  value_t a = args[0];
   ibchar(numval(a));  /*Learn: Lisp num -> C int*/
-  return FL_NIL;}
+  return FL_T;
+
+}
 int keywrdcolor = 1 ;
 
 static value_t forwrd(value_t *args, u_int32_t nargs) {
+
   argcount("forward", nargs, 1);
-  value_t a= args[0];
+  value_t a = args[0];
   ifchar(numval(a));   /*Learn: Lisp num -> C int*/
-  return FL_NIL;}
-
-extern void lnbegin (void);
-extern void lnend (void);
-
-static value_t linebegin(value_t *args, u_int32_t nargs) {
-  argcount("beginning-of-line", nargs, 0);
-  lnbegin();  
-  return FL_NIL;}
-
-static value_t lineend(value_t *args, u_int32_t nargs) {
-  argcount("end-of-line", nargs, 0);
-  lnend();
-  return FL_NIL;}
-
-
-extern void copy(void);
-
-static value_t copy_region(value_t *args, u_int32_t nargs) {
-  argcount("copy_region", nargs, 0);
-  copy();
-  return FL_T;}
-
-
-extern void  delete_other_windows(void);
-
-static value_t del_other_windows(value_t *args, u_int32_t nargs) {
-  argcount("copy_region", nargs, 0);
-  delete_other_windows();
-  return FL_T;}
-
-
-extern void goto_line(int line);
-
-static value_t gotoln(value_t *args, u_int32_t nargs) {
-  argcount("goto-line", nargs, 1);
-  value_t a= args[0];
-  goto_line(numval(a));   /*Learn: Lisp num -> C int*/
-  return FL_T;}
-
-
-extern void cut(void);
-
-static value_t kill_region (value_t *args ,u_int32_t nargs){
-  argcount("kill-region",nargs,0);
-  cut();
   return FL_T;
-  
 
 }
 
+extern void lnbegin (void);
+
+extern void lnend (void);
+
+static value_t linebegin(value_t *args, u_int32_t nargs)
+{
+  argcount("beginning-of-line", nargs, 0); /* Learn: Take no Args */
+  lnbegin();  /* Learn: C funcion call to lisp */ 
+  return FL_T;
+
+}
+
+static value_t lineend(value_t *args, u_int32_t nargs)
+{
+  argcount("end-of-line", nargs, 0);
+  lnend();
+  return FL_T;
+
+}
+
+extern void copy(void);
+
+static value_t copy_region(value_t *args, u_int32_t nargs)
+{
+  argcount("copy_region", nargs, 0);
+  copy();
+  return FL_T;
+
+}
+
+extern void  delete_other_windows(void);
+
+static value_t del_other_windows(value_t *args, u_int32_t nargs)
+{
+  argcount("copy_region", nargs, 0);
+  delete_other_windows();
+  return FL_T;
+
+}
+
+extern void goto_line(int line);
+
+static value_t gotoln(value_t *args, u_int32_t nargs)
+{
+  argcount("goto-line", nargs, 1);
+  value_t a = args[0]; /*Learn: Take arg from Lisp */
+  goto_line(numval(a));   /*Learn: Lisp num -> C int*/
+  return FL_T;
+
+}
+
+extern void cut(void);
+
+static value_t kill_region (value_t *args ,u_int32_t nargs)
+{
+  argcount("kill-region", nargs, 0);
+  cut();
+  return FL_T; 
+
+}
 
 extern void list_buffers(void);
 
-static value_t lst_buffers(value_t *args, u_int32_t nargs){  
+static value_t lst_buffers(value_t *args, u_int32_t nargs)
+{  
   argcount("list-buffers",nargs,0);
   list_buffers();
   return FL_T;
 
 }
 
-
 extern void down(void);
-static value_t next_line(value_t *args, u_int32_t nargs){
-  argcount("next-line",nargs,0);
+
+static value_t next_line(value_t *args, u_int32_t nargs)
+{
+  argcount("next-line", nargs, 0);
   down();
   return FL_T;
 
 }
 
 extern void up(void);
-static value_t previous_line(value_t *args, u_int32_t nargs){
-  argcount("previous-line",nargs,0);
+
+static value_t previous_line(value_t *args, u_int32_t nargs)
+{
+  argcount("previous-line", nargs, 0);
   up();
   return FL_T;
 
 }
 
-
-
 extern point_t search_forward(char *);
+
 extern void display_search_result(point_t, int, char *, char *);
+
 extern point_t search_backwards(char *);
 
-static value_t src_forward(value_t *args,u_int32_t nargs){
-
+static value_t src_forward(value_t *args,u_int32_t nargs)
+{
   point_t founded ;
   argcount("search-forward", nargs, 1);
-  value_t a= args[0];
-  char *str= cptr(a);
+  value_t a = args[0];
+  char *str = cptr(a);
   founded = search_forward(str);
   display_search_result(founded, FWD_SEARCH, m_sprompt,str);
   return FL_T;
+
 }
 
-
-static value_t src_backwards(value_t *args,u_int32_t nargs){
-
+static value_t src_backwards(value_t *args,u_int32_t nargs)
+{
   point_t founded ;
   argcount("search-backwards", nargs, 1);
-  value_t a= args[0];
-  char *str= cptr(a);
+  value_t a = args[0];
+  char *str = cptr(a);
   founded = search_backwards(str);
   display_search_result(founded, 2, m_sprompt,str);
   return FL_T;
+
 }
 
-
-
 extern void iblock(void);
-static value_t set_mark (value_t *args ,u_int32_t nargs){
-  argcount("set-mark",nargs,0);
+
+static value_t set_mark (value_t *args ,u_int32_t nargs)
+{
+  argcount("set-mark", nargs, 0);
   iblock();
   return FL_T;
 
 }
 
-
-
 extern void split_window(void);
-static value_t split_current_window(value_t *args,
-				    u_int32_t nargs){
 
-  argcount("split-current-window",nargs,0);
+static value_t split_current_window(value_t *args,
+				    u_int32_t nargs)
+{
+  argcount("split-current-window", nargs, 0);
   split_window();
   return FL_T;
   
-  }
+}
 
 extern void paste(void);
-static value_t yank(value_t *args,u_int32_t nargs){
 
-  argcount("yank",nargs,0);
+static value_t yank(value_t *args,u_int32_t nargs)
+{
+  argcount("yank", nargs, 0);
   paste();
   return FL_T;
-  
-
 
 }
 
@@ -231,37 +264,51 @@ int numWords[100];
 char hiLite[10][100][30];
 
 
-int kwrd(char_t *p) {
+int kwrd(char_t *p)
+{
   int i, j;
   if (thisLanguage > -1) {
     for (i=0; i < numWords[thisLanguage]; i++) {
       for (j= 0; j <= strlen(hiLite[thisLanguage][i]); j++) {
-        if (j == strlen(hiLite[thisLanguage][i])) return j;
-        else if ( *(p+j) != (char_t) hiLite[thisLanguage][i][j]) break;}}}
-  return 0;}
+        if (j == strlen(hiLite[thisLanguage][i]))
+	  return j;
+        else if ( *(p+j) != (char_t) hiLite[thisLanguage][i][j])
+	  break;
+      }
+    }
+  }
+  return 0;
+
+}
 
 
-void setLanguage(char *extension) {
+void setLanguage(char *extension)
+{
   int i;
   thisLanguage= -1;
   for (i=0; i<nLangs; i++) 
     if (strcmp(LangCode[i], extension)== 0) {
-      thisLanguage= i;}}
+      thisLanguage= i;
+    }
+
+}
 
 static value_t fl_newlanguage(value_t *args, u_int32_t nargs)
 {  
-    argcount("newlanguage", nargs, 1);
-    value_t a = args[0];
+     argcount("newlanguage", nargs, 1);
+     value_t a = args[0];
      int code = nLangs;
      char *str = cptr(a);
      int sz= strlen(str);
      int i;
      for (i=0; i < sz; i++){
-      LangCode[code][i]= str[i];}
+          LangCode[code][i]= str[i];
+     }
      LangCode[code][i]='\0';
      numWords[code]= 0;
      nLangs= nLangs+1;
-    return a;
+     return a;
+
 }
 
 static value_t fl_keyword(value_t *args, u_int32_t nargs)
@@ -269,28 +316,29 @@ static value_t fl_keyword(value_t *args, u_int32_t nargs)
     argcount("keyword", nargs, 1);
     value_t a = args[0];
     int code = nLangs-1;
-    int nw= numWords[code];
+    int nw = numWords[code];
     char *str = cptr(a);
-     int sz= strlen(str);
-     int i;
-     for (i=0; i < sz; i++){
-       hiLite[code][nw][i]= str[i];}
-     hiLite[code][nw][i]='\0';
-     numWords[code]= numWords[code]+1;
+    int sz = strlen(str);
+    int i;
+    for (i=0; i < sz; i++){
+       hiLite[code][nw][i]= str[i];
+    }
+     hiLite[code][nw][i] ='\0';
+     numWords[code] = numWords[code]+1;
     return a;
+
 }
-
-
-
 
 /* END_OF Interface to the editor */
 
 
 extern void stringfuncs_init(void);
+
 extern void table_init(void);
+
 extern void iostream_init(void);
 
-static builtinspec_t builtin_info[] = {
+static builtinspec_t builtin_info[] ={
 
     /*Interface to the editor*/
     {"insert", insrt},
@@ -321,4 +369,5 @@ static builtinspec_t builtin_info[] = {
 void interface_init(void)
 {
     assign_global_builtins(builtin_info);
+    
 }
