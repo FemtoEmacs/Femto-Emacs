19a0a91ce711758e5c32f859baf18dc9496df3bb
Hugh Barney <hughbarney@gmail.com>
2016-11-01 23:38:04 +0000

    added test file for testing syntax highlghting changes

---
+++ docs/hltest.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+//line comment
+
+/*
+ * block comment
+ *
+ */
+
+//setThis();  //commented out code
+
+//example keyword
+void func();
+char s[] = "a string";
+char c;
+
+ptr->m_fish = 0.0;
+
+//1234567890
+
+// all of these special cases should work or we should not attempt to highlght strings
+c='"';   //a single quoted double quotes should work
+c='\'';
+s="'";  //a double quoted single quotes
+s="\"";  //should work
+
+int x = 1234567;
+
+s= "symbols inside a string !£$%^&**()_+={}[]:;@'~#<,>;//.?/\\|";
+xx="digits inside a string 0123456789 should show in the string color";
+
+s="£";
+s=":";
+
